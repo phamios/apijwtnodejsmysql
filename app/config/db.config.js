06a38d2fe245ef5ee0c97c5cacbeca6464b8ef87
Son Pham <sonpxvn@gmail.com>
2020-08-28 17:36:16 +0700

    update

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -3,7 +3,7 @@ module.exports = {
   USER: "b53b02b8ca0565",
   PASSWORD: "2d62a7ea",
   DB: "heroku_64c2a3ffbbc6876",
-  dialect: "mysql",
+  // dialect: "mysql",
   pool: {
     max: 5,
     min: 0,
